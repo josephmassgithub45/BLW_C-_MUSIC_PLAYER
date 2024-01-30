0761ec0a123b5aad90a1e09fdff130b0f22977e9
josephmassgithub45 <JOSEPHMASSAQUOI45@GMAIL.COM>
2024-01-30 00:13:19 +0000

    FIRST COMMIT

--- BLW Music Player/Form1.cs
+++ BLW Music Player/Form1.cs
@@ -19,12 +19,45 @@ namespace BLW_Music_Player
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            axWindowsMediaPlayer1.URL = paths[listBox1.SelectedIndex];
+            
+        }
+
+        String[] paths, files;
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
 
         }
 
         private void btnselectsongs_Click(object sender, EventArgs e)
         {
-
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files = ofd.SafeFileNames;
+                paths = ofd.FileNames;
+                for (int i = 0; i < files.Length; i++)
+                {
+                    listBox1.Items.Add(files[i]);
+                }
+            }
         }
     }
 }
